d5b007f7d4b182046b2608d7fb2ccefe4bd36924
Simon Wanner <skyrising@pvpctutorials.de>
2019-05-04 22:20:16 +0200

    Port plugin message networking code from 1.12

--- src/main/java/quickcarpet/QuickCarpet.java
+++ src/main/java/quickcarpet/QuickCarpet.java
@@ -7,15 +7,18 @@ import net.minecraft.server.command.ServerCommandSource;
 import quickcarpet.commands.*;
 import quickcarpet.helper.TickSpeed;
 import quickcarpet.logging.LoggerRegistry;
+import quickcarpet.network.PluginChannelManager;
 import quickcarpet.utils.HUDController;
 
 public class QuickCarpet implements ModInitializer {
 
     public static MinecraftServer minecraft_server;
+    public static PluginChannelManager pluginChannels;
 
     public static void init(MinecraftServer server) //Constructor of this static single ton class
     {
-        QuickCarpet.minecraft_server = server;
+        minecraft_server = server;
+        pluginChannels = new PluginChannelManager(server);
     }
 
     public static void onServerLoaded(MinecraftServer server) {
